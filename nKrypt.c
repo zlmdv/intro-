75cfcc8a178479c1ae415b8ad3eff0d9c38beb73
Paul <pruggiero04@gmail.com>
2022-03-29 08:38:27 -0400

    nkrypt

---
+++ nKrypt.c
@@ -0,0 +1,60 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
+
+int encrypt(FILE *input, FILE *output, char *password); 
+
+
+int main(int argc, char **argv){
+    char password[] = "vhugnskpteadnhgfkencjsfjjenc";
+    FILE *input = fopen(argv[1], "r"); 
+    FILE *output = fopen(argv[2], "w"); 
+
+    if(input == NULL){
+        printf("File does not exist");
+        return 1;
+    }
+    if(output == NULL){
+        printf("Error opening encrypted file");
+        return 1; 
+    }
+    encrypt(input, output, password); 
+
+    fclose(input); 
+    fclose(output); 
+    return 0; 
+}
+
+/**
+ * @brief function to encrypt text with an xor operation
+ * 
+ * @param input input file
+ * @param output output file
+ * @param password key for encryption
+ * @return int 
+ */
+
+int encrypt(FILE *input,FILE *output,char *password){ 
+
+    unsigned long int filesize;
+    char *buffer;
+    int length;
+
+    length = strlen(password); 
+    fseek(input, 0, SEEK_END); 
+    filesize=ftell(input);
+    buffer = malloc(filesize);      // this whole block of  code gets file siz3
+    rewind(input); 
+    printf("read %u\n", filesize);
+    fread(buffer, 1, filesize, input); 
+    for(int i = 0; i < filesize; i ++){
+        buffer[i] = buffer[i] ^ password[i % length]; //executes xor operation     
+    }
+    int w = fwrite(buffer, 1, filesize, output );  //writes data to output file
+    printf("written %d", w);      
+    free(buffer); 
+}
+
+//TODO: write a decryption function and ad more command line args
